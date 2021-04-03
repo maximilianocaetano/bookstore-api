855f6a9f7e11dcfbf5bc1837a13d5ad4e7d73a4d
maximilianocaetano <maximilianocaetano@gmail.com>
2021-04-03 02:19:51 -0300

    commit inicial

---
+++ src/main/java/com/max/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,16 @@
+package com.max.bookstore.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.max.bookstore.domain.Livro;
+
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+
+	@Query("SELECT obj FROM Livro obj WHERE obj.categoria.id = :id_cat ORDER BY titulo")
+	List<Livro> findAllByCategoria(@Param( value = "id_cat") Integer id_cat);
+
+}
---
+++ src/main/java/com/max/bookstore/service/CategoriaService.java
@@ -0,0 +1,54 @@
+package com.max.bookstore.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.max.bookstore.domain.Categoria;
+import com.max.bookstore.dtos.CategoriaDTO;
+import com.max.bookstore.repositories.CategoriaRepository;
+import com.max.bookstore.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repository;
+
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado!  Id:" + id + ", Tipo: " + Categoria.class.getName()));
+
+	}
+
+	public List<Categoria> findAll() {
+		return repository.findAll();
+	}
+
+	public Categoria create(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
+		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.max.bookstore.service.exceptions.DataIntegrityViolationException
+			("Categoria não pode ser deletada! Possui Livros associados!");
+		}
+
+	}
+}
